fe3eead1a9c32284354a6ab7b2a5841c93b3c53d
Kristin Dailey <kristin.dailey02@gmail.com>
2024-10-24 20:43:22 -0700

    removed deprecated mongoose options

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo");
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
@@ -11,14 +10,16 @@ const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 
-//Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
+//Use config.env file in config folder
+require("dotenv").config({ path: "./config/config.env" });
 
 // Passport config
 require("./config/passport")(passport);
 
 //Connect To Database
 connectDB();
+
+const app = express();
 
 //Using EJS for views
 app.set("view engine", "ejs");
@@ -42,8 +43,8 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
-  })
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING })
+    }),
 );
 
 // Passport middleware
